abd8167a105ff5ef32c593746210abf86a6e9581
Gennady <g.korolew@gmail.com>
2025-01-02 22:57:38 +0400

    написан тест

--- src/test/java/ru/netology/SpringBootDemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,41 @@
 package ru.netology.SpringBootDemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class SpringBootDemoApplicationTests {
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Container
+	private static final GenericContainer<?> containerDev = new GenericContainer<>("devapp")
+			.withExposedPorts(8080);
+
+	@Container
+	private static final GenericContainer<?> containerProd = new GenericContainer<>("prodapp")
+			.withExposedPorts(8081);
 
 	@Test
 	void contextLoads() {
-	}
+		Integer devAppPort = containerDev.getMappedPort(8080);
+		Integer prodAppPort = containerProd.getMappedPort(8081);
 
+		ResponseEntity<String> forEntityDev = restTemplate.getForEntity("http://localhost:" + devAppPort + "/profile", String.class);
+		ResponseEntity<String> forEntityProd = restTemplate.getForEntity("http://localhost:" + prodAppPort + "/profile", String.class);
+
+		System.out.println(forEntityDev.getBody());
+		System.out.println(forEntityProd.getBody());
+
+		Assertions.assertEquals("Current profile is dev", forEntityDev.getBody());
+		Assertions.assertEquals("Current profile is production", forEntityProd.getBody());
+	}
 }
