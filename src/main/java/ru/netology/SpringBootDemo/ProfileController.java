5cea9a67ef6bd22e56ea4b4d1c4c95cb8bc3d87a
Gennady <g.korolew@gmail.com>
2025-01-06 18:47:15 +0400

    корректировка в классе ProfileController v1

--- src/main/java/ru/netology/SpringBootDemo/ProfileController.java
+++ src/main/java/ru/netology/SpringBootDemo/ProfileController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConfigurationProperties
+@ConfigurationProperties ("profile")
 @RequestMapping("/")
 public class ProfileController {
     private SystemProfile profile;
